2f77d1dd317618801b6c1ffdecb49de91ddea9f3
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-10-24 23:59:32 -0700

    Creating class

---
+++ sam_m8q_module/sam_m8q.h
@@ -0,0 +1,20 @@
+#ifndef SAM_M8Q
+#define SAM_M8Q
+
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <map>
+
+class SAM_M8Q {
+  public:
+	
+	// Constructor
+	// Methods
+  
+  private:
+
+}
+
+#endif // SAM_M8Q
