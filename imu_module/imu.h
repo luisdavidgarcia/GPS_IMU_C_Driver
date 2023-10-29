921c7bc57b9dda46709051b1fcc9111f99ad445f
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-10-29 08:50:04 -0700

    IMU header file created

---
+++ imu_module/imu.h
@@ -0,0 +1,28 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <sys/ioctl.h>
+#include <time.h>
+
+class Imu {
+private:
+    int i2c_fd;
+    float accelerometer[3];
+    float magnetometer[3];
+    float gyroscope[3];
+
+    void readSensorData();
+
+public:
+    Imu();
+    ~Imu();
+    void printAccel();
+    void printMag();
+    void printGyro();
+    void Telementary(int delay);
+};
+
+#endif // IMU_H
+
