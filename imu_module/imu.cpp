cda68edee9710bab0760ccf518c09c2069203a17
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-10-29 08:57:15 -0700

    Added print functions for accelerometer, magnometer, and gyroscope

--- imu_module/imu.cpp
+++ imu_module/imu.cpp
@@ -18,3 +18,27 @@ Imu::Imu() {
 Imu::~Imu() {
     close(i2c_fd);
 }
+
+/**
+ * @brief   Print accelerometer data to console.
+ */
+void Imu::printAccel() {
+    printf("Acceleration (m/s^2): (%0.3f, %0.3f, %0.3f)\n",
+           accelerometer[0], accelerometer[1], accelerometer[2]);
+}
+
+/**
+ * @brief   Print magnetometer data to console.
+ */
+void Imu::printMag() {
+    printf("Magnetometer (uTesla): (%0.3f, %0.3f, %0.3f)\n",
+           magnetometer[0], magnetometer[1], magnetometer[2]);
+}
+
+/**
+ * @brief   Print gyroscope data to console.
+ */
+void Imu::printGyro() {
+    printf("Gyroscope (radians/s): (%0.3f, %0.3f, %0.3f)\n",
+           gyroscope[0], gyroscope[1], gyroscope[2]);
+}
