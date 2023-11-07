dcf8a7a8658cdc1dc30512640fe698e41ab64309
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-07 09:53:30 -0800

    Updated tests

--- tests/imu_tests/test_imu.cpp
+++ tests/imu_tests/test_imu.cpp
@@ -134,7 +134,7 @@ int main() {
     mag_z_h = i2c_smbus_read_byte_data(i2c_file, 0x40);
     mag_z_l = i2c_smbus_read_byte_data(i2c_file, 0x41);
 
-    int16_t mag_x = (mag_x_h << 8) | (mag_x_l & 0xFF);
+    int16_t mag_x = (mag_x_l << 8) | (mag_x_h & 0xFF);
     int16_t mag_y = (mag_y_h << 8) | (mag_y_l & 0xFF);
     int16_t mag_z = (mag_z_h << 8) | (mag_z_l & 0xFF);
 
