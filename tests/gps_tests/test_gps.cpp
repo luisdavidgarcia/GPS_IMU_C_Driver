36fcb7ecb2b520c36975589764f8acf0908a4ee5
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-13 15:43:28 -0800

    resolved bugs

--- tests/gps_tests/test_gps.cpp
+++ tests/gps_tests/test_gps.cpp
@@ -8,6 +8,12 @@ int main(void) {
     PVTData data = gps_module.GetPvt(true, 1);
     if (data.year == 2023) {
       printf("Year: %d\n", data.year);
+      printf("Month: %d\n", data.month);
+      printf("Day: %d\n", data.day);  
+      printf("Hour: %d\n", data.hour);
+      printf("Min: %d\n", data.min);
+      printf(
+          "Sec: %d\n", data.sec);  // TODO: Convert to seconds with decimal
     }
     sleep(1);
   }
