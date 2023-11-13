a524b5ae5fd9ed89c1e8e611b6cee5db0311564b
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-13 15:13:47 -0800

    resolved bugs

--- gps_module/gps.cpp
+++ gps_module/gps.cpp
@@ -143,17 +143,17 @@ UbxMessage Gps::readUbxMessage() {
             }
             message.push_back(static_cast<uint8_t>(byte_data)); // Cast to uint8_t
         }
-    }
 
-  //       printf("Message Size: %lu\n", message.size());
-  //       // for (size_t i = 0; i < message.size(); ++i) {
-  //       //     printf("i: %d |", i);
-  //       //     printf("0x%02X", message[i]); // %02X formats the number in hexadecimal, 2 digits with leading zeros
-  //       //     // if (i < message.size() - 1) {
-  //       //     //     printf(", "); // Separate elements with a comma
-  //       //     // }
-  //       // }
-  //       printf("\n-----------------------"); // New line after printing all elements
+        printf("Message Size: %lu\n", message.size());
+        for (size_t i = 0; i < message.size(); ++i) {
+            printf("i: %d |", i);
+            printf("0x%02X", message[i]); // %02X formats the number in hexadecimal, 2 digits with leading zeros
+            if (i < message.size() - 1) {
+                printf(", "); // Separate elements with a comma
+            }
+        }
+        printf("\n-----------------------"); // New line after printing all elements
+    }
 
   //       UbxMessage ubxMsg;
   //       ubxMsg.sync1 = message[0];
