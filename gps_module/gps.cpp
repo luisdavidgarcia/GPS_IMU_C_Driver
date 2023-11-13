1e0b08e5cd1332843d43e84ca4b2d160476ee7c0
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-13 14:19:19 -0800

    resolved bugs

--- gps_module/gps.cpp
+++ gps_module/gps.cpp
@@ -105,18 +105,18 @@ bool Gps::writeUbxMessage(UbxMessage &msg) {
     buf[i] = tempBuf[i];
   }
 
-  // for (int i = 0; i < tempBuf.size(); i++) {
-  //   int8_t reg = i2c_smbus_write_byte(i2c_fd, buf[i]);
-  //   if (reg < 0) {
-  //     perror("Failed to write to I2C device");
-  //     return false;
-  //   }
-  // }
-
-  if (i2c_smbus_write_block_data(i2c_fd, 0xFF, tempBuf.size(), buf) < 0) {
-    perror("Failed to write to I2C device");
-    return false;
+  for (int i = 0; i < tempBuf.size(); i++) {
+    int8_t reg = i2c_smbus_write_byte_data(i2c_fd, 0xFF, buf[i]);
+    if (reg < 0) {
+      perror("Failed to write to I2C device");
+      return false;
+    }
   }
+
+  // if (i2c_smbus_write_block_data(i2c_fd, 0xFF, tempBuf.size(), buf) < 0) {
+  //   perror("Failed to write to I2C device");
+  //   return false;
+  // }
 
   return true;
 }
