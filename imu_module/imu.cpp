86232727d075f6ba4e32d63b38d2f4a2e016e676
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-18 15:18:38 -0800

    Updates to the IMU documentation

--- imu_module/imu.cpp
+++ imu_module/imu.cpp
@@ -3,79 +3,93 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+/**
+ * @brief   Constructor for the Imu class.
+ *
+ * Initializes the I2C communication with the IMU sensor, sets its address, and performs an initial identification check.
+ */
 Imu::Imu() {
-  const char *deviceName = IMU_I2C_BUS;
-  i2c_fd = open(deviceName, O_RDWR);
-  if (i2c_fd < 0) {
-    perror("Unable to open I2C device");
-  }
+	const char *deviceName = IMU_I2C_BUS;
+	i2c_fd = open(deviceName, O_RDWR);
+	if (i2c_fd < 0) {
+		perror("Unable to open I2C device");
+	}
 
-  if (ioctl(i2c_fd, I2C_SLAVE, IMU_I2C_ADDRESS) < 0) {
-    perror("Failed to acquire bus access and/or talk to slave");
-  }
+	if (ioctl(i2c_fd, I2C_SLAVE, IMU_I2C_ADDRESS) < 0) {
+		perror("Failed to acquire bus access and/or talk to slave");
+	}
 
-  // Check ID
-  if (i2c_smbus_read_byte_data(i2c_fd, 0x00) != 0xEA) {
-    perror("Failed to identify chip");
-  }
+	if (i2c_smbus_read_byte_data(i2c_fd, 0x00) != IMU_ID) {
+		perror("Failed to identify chip");
+	}
 
-  begin();
+	begin();
 }
 
+/**
+ * @brief   Destructor for the Imu class.
+ *
+ * Closes the I2C device when the Imu object is destroyed.
+ */
 Imu::~Imu() { close(i2c_fd); }
 
+
+/**
+ * @brief   Initialize the IMU sensor configuration.
+ *
+ * Sets up the IMU sensor by configuring registers and settings
+ * for accelerometer, gyroscope, and magnetometer communication.
+ */
 void Imu::begin() {
-  // TODO: Create error_handler for register writes
-  
-  // Select Clock to Automatic (Init Accel and Gyro)
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x06, 0x01);
+	// Select Clock to Automatic (Init Accel and Gyro)
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x06, 0x01);
 
-  /* Init Magnometer */
-  // Master Pass Through set to false (For Magnometer)
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x0F, 0x00); 
+	/* Init Magnometer */
+	// Master Pass Through set to false (For Magnometer)
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x0F, 0x00);
 
-  // Enable Master (For Magnometer)
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x01, 0x17);
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x03, 0x20); 
+	// Enable Master (For Magnometer)
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x01, 0x17);
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x03, 0x20);
 
-  // Transact directly with an I2C device, one byte at a time (For Magnometer)
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x13, 0x0C); 
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x14, 0x31); 
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x16, 0x08); 
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x15, 0x80); 
+	// Transact directly with an I2C device, one byte at a time (For Magnometer)
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x13, 0x0C);
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x14, 0x31);
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x16, 0x08);
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x15, 0x80);
 
-  // Set up Slaves with Master (For Magnometer)
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
-  i2c_smbus_write_byte_data(i2c_fd, 0x03, 0x8C); 
-  i2c_smbus_write_byte_data(i2c_fd, 0x04, 0x10); 
-  i2c_smbus_write_byte_data(i2c_fd, 0x05, 0x89); 
+	// Set up Slaves with Master (For Magnometer)
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x30); //set bank
+	i2c_smbus_write_byte_data(i2c_fd, 0x03, 0x8C);
+	i2c_smbus_write_byte_data(i2c_fd, 0x04, 0x10);
+	i2c_smbus_write_byte_data(i2c_fd, 0x05, 0x89);
 
-  /* Reset Bank to Zero 0 For Reading Data */
-  i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
+	/* Reset Bank to Zero 0 For Reading Data */
+	i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
 }
 
 /**
  * @brief   Read sensor data from IMU over I2C.
  *          Replace this section with your actual I2C communication to read
- * sensor data. For example: accelerometer[0] = <read accelerometer X value>;
+ * 			sensor data. For example: accelerometer[0] = <read accelerometer X value>;
  *          accelerometer[1] = <read accelerometer Y value>;
  *          accelerometer[2] = <read accelerometer Z value>;
- *          magnetometer[0] = <read magnetometer X value>;
- *          magnetometer[1] = <read magnetometer Y value>;
- *          magnetometer[2] = <read magnetometer Z value>;
  *          gyroscope[0] = <read gyroscope X value>;
  *          gyroscope[1] = <read gyroscope Y value>;
  *          gyroscope[2] = <read gyroscope Z value>;
+ *          magnetometer[0] = <read magnetometer X value>;
+ *          magnetometer[1] = <read magnetometer Y value>;
+ *          magnetometer[2] = <read magnetometer Z value>;
  */
-void Imu::readSensorData() {
+void Imu::readSensorData(void) {
     /* Reset bank to 0 on every read */
     i2c_smbus_write_byte_data(i2c_fd, 0x7F, 0x00); //set bank
 
@@ -88,7 +102,7 @@ void Imu::readSensorData() {
     accel_z_h = i2c_smbus_read_byte_data(i2c_fd, 0x31);
     accel_z_l = i2c_smbus_read_byte_data(i2c_fd, 0x32);
 
-    // Converting Raw Accel Data to Readable data 
+    // Converting Raw Accel Data to Readable data
     accelerometer[0] = (accel_x_h << 8) | (accel_x_l & 0xFF);
     accelerometer[1] = (accel_y_h << 8) | (accel_y_l & 0xFF);
     accelerometer[2] = (accel_z_h << 8) | (accel_z_l & 0xFF);
@@ -102,13 +116,13 @@ void Imu::readSensorData() {
     gyro_z_h = i2c_smbus_read_byte_data(i2c_fd, 0x37);
     gyro_z_l = i2c_smbus_read_byte_data(i2c_fd, 0x38);
 
-    // Converting Raw Gyro Data to Readable data 
+    // Converting Raw Gyro Data to Readable data
     gyroscope[0] = (gyro_x_h << 8) | (gyro_x_l & 0xFF);
     gyroscope[1] = (gyro_y_h << 8) | (gyro_y_l & 0xFF);
     gyroscope[2] = (gyro_z_h << 8) | (gyro_z_l & 0xFF);
 
     /* Read magentometer data */
-    uint8_t mag_x_h, mag_x_l, mag_y_h, mag_y_l, mag_z_h, mag_z_l;
+	uint8_t mag_x_h, mag_x_l, mag_y_h, mag_y_l, mag_z_h, mag_z_l;
     mag_x_h = i2c_smbus_read_byte_data(i2c_fd, 0x3C);
     mag_x_l = i2c_smbus_read_byte_data(i2c_fd, 0x3D);
     mag_y_h = i2c_smbus_read_byte_data(i2c_fd, 0x3E);
@@ -116,7 +130,7 @@ void Imu::readSensorData() {
     mag_z_h = i2c_smbus_read_byte_data(i2c_fd, 0x40);
     mag_z_l = i2c_smbus_read_byte_data(i2c_fd, 0x41);
 
-    // Converting Raw Mag Data to Readable data 
+    // Converting Raw Mag Data to Readable data
     magnetometer[0] = (mag_x_l << 8) | (mag_x_h & 0xFF);
     magnetometer[1] = (mag_y_l << 8) | (mag_y_h & 0xFF);
     magnetometer[2] = (mag_z_l << 8) | (mag_z_h & 0xFF);
@@ -125,40 +139,45 @@ void Imu::readSensorData() {
 /**
  * @brief   Print accelerometer data to console.
  */
-void Imu::printAccel() {
-  printf("Acceleration (m/s^2): (%d, %d, %d)\n", accelerometer[0],
-         accelerometer[1], accelerometer[2]);
+void Imu::printAccel(void) {
+  	printf("Acceleration (m/s^2): (%d, %d, %d)\n", accelerometer[0],
+        accelerometer[1], accelerometer[2]);
 }
 
 /**
  * @brief   Print magnetometer data to console.
  */
-void Imu::printMag() {
-  printf("Magnetometer (uTesla): (%d, %d, %d)\n", magnetometer[0],
-         magnetometer[1], magnetometer[2]);
+void Imu::printMag(void) {
+  	printf("Magnetometer (uTesla): (%d, %d, %d)\n", magnetometer[0],
+        magnetometer[1], magnetometer[2]);
 }
 
 /**
  * @brief   Print gyroscope data to console.
  */
-void Imu::printGyro() {
-  printf("Gyroscope (radians/s): (%d, %d, %d)\n", gyroscope[0],
-         gyroscope[1], gyroscope[2]);
+void Imu::printGyro(void) {
+  	printf("Gyroscope (radians/s): (%d, %d, %d)\n", gyroscope[0],
+    	gyroscope[1], gyroscope[2]);
 }
 
 /**
  * @brief   Continuously print sensor data with a specified delay.
+ *
+ * Continuously prints sensor data, including accelerometer,
+ * gyroscope, and magnetometer data, to the console with a specified delay
+ * in milliseconds.
+ *
  * @param   delay   Delay in milliseconds between data prints.
  */
 void Imu::Telementary(int delay) {
-  while (true) {
-    time_t t = time(NULL);
-    struct tm *tm_info = localtime(&t);
-    printf("%s", asctime(tm_info));
-    readSensorData();
-    printAccel();
-    printGyro();
-    printMag();
-    usleep(delay * TIME_DELAY_MS);
-  }
+	while (true) {
+		time_t t = time(NULL);
+		struct tm *tm_info = localtime(&t);
+		printf("%s", asctime(tm_info));
+		readSensorData();
+		printAccel();
+		printGyro();
+		printMag();
+		usleep(delay * TIME_DELAY_MS);
+	}
 }
--- imu_module/imu.h
+++ imu_module/imu.h
@@ -1,15 +1,48 @@
+/*
+ * imu.h - Header file for IMU (Inertial Measurement Unit) sensor integration
+ *
+ * This header file provides an interface for interacting with an IMU sensor over
+ * the I2C communication protocol. It includes functions and constants for retrieving
+ * accelerometer, gyroscope, and magnetometer data from the IMU sensor. The code is
+ * designed for a specific IMU sensor and may require configuration adjustments for
+ * different hardware or use cases.
+ *
+ * CREDIT/CODE MODIFIED FROM:
+ * https://github.com/GoScoutOrg/Rover/blob/749a7758aef85ed877ad6db56e223f91a708abdf/src/rover/imu.py
+ *
+ * Dependencies:
+ * - i2c/smbus.h, linux/i2c-dev.h, linux/i2c.h: Required for I2C communication.
+ * - cstdint, sys/ioctl.h, time.h: Standard C++ libraries.
+ *
+ * Constants and Definitions:
+ * - Various constants for I2C addresses, register addresses, data sizes, and sensitivities.
+ *
+ * Class:
+ * - Imu: A class that encapsulates IMU functionality, including I2C communication,
+ *   sensor data retrieval, and telemetry functions.
+ *
+ * Usage:
+ * - Include this header file in your C++ project to interact with an IMU sensor.
+ * - Instantiate the Imu class to communicate with the IMU sensor and retrieve data.
+ *
+ * Note: This code is designed for a specific IMU sensor and may require adaptation for
+ *       other IMU sensors or hardware configurations. Refer to the provided credit and
+ *       documentation for further details on usage and customization.
+ */
+
 #ifndef IMU_H
 #define IMU_H
 
 extern "C" {
-#include <i2c/smbus.h>
-#include <linux/i2c-dev.h>
-#include <linux/i2c.h>
+	#include <i2c/smbus.h>
+	#include <linux/i2c-dev.h>
+	#include <linux/i2c.h>
 }
 #include <cstdint>
 #include <sys/ioctl.h>
 #include <time.h>
 
+/** IMU Constants */
 #define TIME_DELAY_MS 1000
 #define ACCEL_REG_START 0x00
 #define MAGNETO_REG_START 0x00
@@ -18,9 +51,14 @@ extern "C" {
 #define BYTE_SHIFT_AMOUNT 8
 #define BANK_VALUE 0x02
 #define PWR_MGMT_2_VALUE 0x000000
+#define X_AXIS 0
+#define Y_AXIS 1
+#define Z_AXIS 2
 
+/** I2C Specifics */
 #define IMU_I2C_ADDRESS 0x69
 #define IMU_I2C_BUS "/dev/i2c-1"
+#define IMU_ID 0xEA
 
 /** General Registers */
 #define BANK_SEL 0x7F
@@ -49,24 +87,24 @@ extern "C" {
 #define ACCEL_MG_LSB_8G (0.000976F)
 
 /** Macro for micro tesla (uT) per LSB (1 LSB = 0.1uT) */
-#define MAG_UT_LSB (0.1F)
+#define MAG_UT_LSB (0.1R)
 
 class Imu {
 private:
-  int i2c_fd;
-  int16_t accelerometer[3];
-  int16_t magnetometer[3];
-  int16_t gyroscope[3];
-  void begin(void);
-  void readSensorData(void);
+	int i2c_fd;
+	int16_t accelerometer[3];
+	int16_t magnetometer[3];
+	int16_t gyroscope[3];
+	void begin(void);
+	void readSensorData(void);
 
 public:
-  Imu();
-  ~Imu();
-  void printAccel();
-  void printMag();
-  void printGyro();
-  void Telementary(int delay);
+	Imu();
+	~Imu();
+	void printAccel(void);
+	void printMag(void);
+	void printGyro(void);
+	void Telementary(int delay);
 };
 
 #endif // IMU_H
