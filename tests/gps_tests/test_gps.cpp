32216df725b94215e6f746c0287352fada16d4de
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-22 23:09:11 -0800

    Improved non-graph imu/gps tests

--- tests/gps_tests/test_gps.cpp
+++ tests/gps_tests/test_gps.cpp
@@ -7,12 +7,14 @@ int main(void) {
   while(1) {
     PVTData data = gps_module.GetPvt(true, 1);
     if (data.year == 2023) {
+      /* Use these times to plot on x-axis */
       printf("Year: %d\n", data.year);
       printf("Month: %d\n", data.month);
-      printf("Day: %d\n", data.day);  
+      printf("Day: %d\n", data.day);
       printf("Hour: %d\n", data.hour);
       printf("Min: %d\n", data.min);
       printf("Sec: %d\n", data.sec);
+      /* Don't Plot These */
       printf("Valid Time Flag: %d\n", data.validTimeFlag);
       printf("Valid Date Flag: %d\n", data.validDateFlag);
       printf("Fully Resolved Flag: %d\n", data.fullyResolved);
@@ -20,12 +22,14 @@ int main(void) {
       printf("GNSS Fix: %d\n", data.gnssFix);
       printf("Fix Status Flags: %d\n", data.fixStatusFlags);
       printf("Number of Satellites: %d\n", data.numberOfSatellites);
+      /* Plot the Long, Lat, and Height */
       printf("Longitude: %d\n", data.longitude);
       printf("Latitude: %d\n", data.latitude);
       printf("Height: %d\n", data.height);
       printf("Height above MSL: %d\n", data.heightMSL);
       printf("Horizontal Accuracy: %u\n", data.horizontalAccuracy);
       printf("Vertical Accuracy: %u\n", data.verticalAccuracy);
+      /* Plot the Velocities and Headings */
       printf("North Velocity: %d\n", data.velocityNorth);
       printf("East Velocity: %d\n", data.velocityEast);
       printf("Down Velocity: %d\n", data.velocityDown);
@@ -34,11 +38,12 @@ int main(void) {
       printf("Motion Heading: %d\n", data.motionHeading);
       printf("Speed Accuracy: %u\n", data.speedAccuracy);
       printf("Motion Heading Accuracy: %d\n", data.motionHeadingAccuracy);
+      /* Don't Plot These */
       printf("Magnetic Declination: %d\n", data.magneticDeclination);
       printf("Magnetic Declination Accuracy: %u\n", data.magnetDeclinationAccuracy);
       printf("\n---------------------\n");
     } else {
-      printf("No data\n"); 
+      printf("No data\n");
     }
     sleep(1);
   }
--- tests/imu_tests/test_imu.cpp
+++ tests/imu_tests/test_imu.cpp
@@ -1,5 +1,5 @@
 #include "../../imu_module/imu.h"
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -14,8 +14,12 @@ extern "C" {
 }
 
 int main(void) {
-  Imu imu_module; 
+  Imu imu_module;
   imu_module.Telementary(1000);
+  imu_readSensorData();
+  const int16_t *accel_data = imu_module.getAccelerometerData();
+  const int16_t *gyro_data = imu_module.getGyroscopeData();
+  const int16_t *mag_data = imu_module.getMagnetometerData();
 
   return 0;
 }
