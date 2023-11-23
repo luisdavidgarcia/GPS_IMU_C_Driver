f80485223c83bf16b74e70900cebcb4256174aa8
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-23 12:11:11 -0800

    resolved bugs

--- tests/graphing/imu_graphs.cpp
+++ tests/graphing/imu_graphs.cpp
@@ -60,19 +60,12 @@ int main() {
 
         // Plot Accelerometer Data
         plotData("Accelerometer Data", time, accel_x, accel_y, accel_z);
-        //plt::clf(); // Clear the current figure
-
-        // plt::named_plot("Accel X", time, accel_x);
-        // plt::named_plot("Accel Y", time, accel_y);
-        // plt::named_plot("Accel Z", time, accel_z);
-
-        // plt::legend(); // Add a legend
 
         // Plot Gyroscope Data
-        // plotData("Gyroscope Data", time, gyro_x, gyro_y, gyro_z);
+        plotData("Gyroscope Data", time, gyro_x, gyro_y, gyro_z);
 
         // Plot Magnetometer Data
-        // plotData("Magnetometer Data", time, mag_x, mag_y, mag_z);
+        plotData("Magnetometer Data", time, mag_x, mag_y, mag_z);
 
         plt::pause(updateInterval); // Update the plot
 
