fbebf259f1d24f87242818426c095cf88f598d77
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-28 13:26:57 -0800

    resolved bugs

--- tests/imu_tests/test_imu.cpp
+++ tests/imu_tests/test_imu.cpp
@@ -9,15 +9,32 @@
 #include <math.h>
 #include <chrono>
 #include <thread>
+#include <csignal>
 
 extern "C" {
 #include <i2c/smbus.h>
 #include <linux/i2c-dev.h>
 }
 
+// Define a flag to indicate if the program should exit gracefully.
+volatile bool exit_flag = false;
+
+// Signal handler function for Ctrl+C (SIGINT)
+void signal_handler(int signum) {
+    if (signum == SIGINT) {
+        std::cout << "Ctrl+C received. Cleaning up..." << std::endl;
+
+        // Set the exit flag to true to trigger graceful exit.
+        exit_flag = true;
+    }
+}
+
 int main(void) {
+  // Register the signal handler for SIGINT (Ctrl+C)
+  signal(SIGINT, signal_handler);
+
   Imu imu_module;
-  for (int i = 0; i < 10; i++) {
+  while (!exit_flag) {
     imu_module.readSensorData();
     // const int16_t *accel_data = imu_module.getAccelerometerData();
     // const int16_t *gyro_data = imu_module.getGyroscopeData();
@@ -28,8 +45,11 @@ int main(void) {
     printf("\n");
     sleep(1);
   }
+    // Perform any necessary cleanup before exiting
+    std::cout << "Exiting program." << std::endl;
 
-  return 0;
+    // Exit the program
+    std::exit(0);
 }
 
 
