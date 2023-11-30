2493262b51e611f06a553f19f689a34c32ab7408
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-11-30 14:59:04 -0800

    resolved bugs

--- tests/graphing/gps_graphs.cpp
+++ tests/graphing/gps_graphs.cpp
@@ -7,7 +7,7 @@ namespace plt = matplotlibcpp;
 int main() {
     Gps gps_module;
     std::vector<double> time;
-    std::vector<int32_t> velocityNorth;
+    std::vector<int32_t> velocityNorth, velocityEast;
 
     double elapsedTime = 0.0;
     const double updateInterval = 1.0; // Update interval in seconds
@@ -21,29 +21,32 @@ int main() {
         if (time.size() > maxDataPoints) {
             time.erase(time.begin());
             velocityNorth.erase(velocityNorth.begin());
+            velocityEast.erase(velocityEast.begin());
         }
 
         // Update the time and data vectors
         time.push_back(elapsedTime);
         velocityNorth.push_back(data.velocityNorth);
+        velocityEast.push_back(data.velocityEast);
 
         // Print type of velocityNorth
-        printf("Type Velocity North: %s\n", typeid(data.velocityNorth).name());
-        printf("RAW Velocity North: %d\n", data.velocityNorth);
+        // printf("Type Velocity North: %s\n", typeid(data.velocityNorth).name());
+        // printf("RAW Velocity North: %d\n", data.velocityNorth);
 
         elapsedTime += updateInterval;
         // Print Last value in velocityNorth
-        printf("Last Velocity North: %d\n", velocityNorth.back());
-        printf("Vector: ");
-        for (int i = 0; i < velocityNorth.size(); i++) {
-            printf("%d , ", velocityNorth[i]);
-        }
-        printf("\n");
+        // printf("Last Velocity North: %d\n", velocityNorth.back());
+        // printf("Vector: ");
+        // for (int i = 0; i < velocityNorth.size(); i++) {
+        //     printf("%d , ", velocityNorth[i]);
+        // }
+        // printf("\n");
 
         plt::figure(1);
         plt::clf(); // Clear the current figure
         plt::title("Velocities");
         plt::named_plot("North Velocity", time, velocityNorth);
+        plt::named_plot("East Velocity", time, velocityEast);
         plt::legend();
         plt::pause(0.1);
 
