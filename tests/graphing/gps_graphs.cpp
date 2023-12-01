16b45d1ccdc2d41b40b90aea916f87382fde63fa
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-12-01 11:27:29 -0800

    resolved bugs

--- tests/graphing/gps_graphs.cpp
+++ tests/graphing/gps_graphs.cpp
@@ -1,73 +1,13 @@
-// #include "../../gps_module/gps.h"
-// #include "matplotlibcpp.h"
-// #include <vector>
-
-// namespace plt = matplotlibcpp;
-
-// int main() {
-//     Gps gps_module;
-//     std::vector<double> time;
-//     //std::vector<int32_t> velocityNorth, velocityEast;
-//     std::vector<double> velocityNorth, velocityEast;
-
-//     double elapsedTime = 0.0;
-//     const double updateInterval = 1.0; // Update interval in seconds
-//     const int maxDataPoints = 100; // Maximum number of points to display on graph
-
-//     plt::figure_size(800, 400); // Adjust the size as needed
-//     plt::ion(); // Turn on interactive mode
-
-//     while (true) {
-//         PVTData data = gps_module.GetPvt(true, 1);
-
-//         if (time.size() > maxDataPoints) {
-//             time.erase(time.begin());
-//             velocityNorth.erase(velocityNorth.begin());
-//             velocityEast.erase(velocityEast.begin());
-//         }
-
-//         // Update the time and data vectors
-//         time.push_back(elapsedTime);
-//         velocityNorth.push_back(data.velocityNorth);
-//         velocityEast.push_back(data.velocityEast);
-
-//         // Print type of velocityNorth
-//         // printf("Type Velocity North: %s\n", typeid(data.velocityNorth).name());
-//         // printf("RAW Velocity North: %d\n", data.velocityNorth);
-
-//         elapsedTime += updateInterval;
-//         // Print Last value in velocityNorth
-//         // printf("Last Velocity North: %d\n", velocityNorth.back());
-//         // printf("Vector: ");
-//         // for (int i = 0; i < velocityNorth.size(); i++) {
-//         //     printf("%d , ", velocityNorth[i]);
-//         // }
-//         // printf("\n");
-
-//         plt::figure(1);
-//         plt::clf(); // Clear the current figure
-//         plt::title("Velocities");
-//         plt::named_plot("North Velocity", time, velocityNorth);
-//         plt::named_plot("East Velocity", time, velocityEast);
-//         plt::legend();
-//         plt::pause(0.5);
-
-//         sleep(1);
-//     }
-
-//     return 0;
-// }
-
 #include "../../gps_module/gps.h"
 #include "matplotlibcpp.h"
 #include <vector>
-#include <unistd.h>  // for sleep
 
 namespace plt = matplotlibcpp;
 
 int main() {
     Gps gps_module;
     std::vector<double> time;
+    //std::vector<int32_t> velocityNorth, velocityEast;
     std::vector<double> velocityNorth, velocityEast;
 
     double elapsedTime = 0.0;
@@ -76,44 +16,44 @@ int main() {
 
     plt::figure_size(800, 400); // Adjust the size as needed
     plt::ion(); // Turn on interactive mode
-    plt::title("Velocities");
-    plt::xlabel("Time");
-    plt::ylabel("Velocity");
 
     while (true) {
         PVTData data = gps_module.GetPvt(true, 1);
 
-        if (time.size() > maxDataPoints) {
-            time.erase(time.begin());
-            velocityNorth.erase(velocityNorth.begin());
-            velocityEast.erase(velocityEast.begin());
-        }
+        // if (time.size() > maxDataPoints) {
+        //     time.erase(time.begin());
+        //     velocityNorth.erase(velocityNorth.begin());
+        //     velocityEast.erase(velocityEast.begin());
+        // }
 
         // Update the time and data vectors
         time.push_back(elapsedTime);
         velocityNorth.push_back(data.velocityNorth);
         velocityEast.push_back(data.velocityEast);
 
-        // Redraw the entire plot with new data
-        plt::clf(); // Clear the current figure
-        plt::title("Velocities");
-        plt::xlabel("Time");
-        plt::ylabel("Velocity");
-
-        // Plot North Velocity in blue
-        plt::plot(time, velocityNorth, "b-");
-
-        // Plot East Velocity in red
-        plt::plot(time, velocityEast, "r-");
-
-        plt::legend();
-        plt::pause(0.01); // Small pause to allow the plot to update
+        // Print type of velocityNorth
+        // printf("Type Velocity North: %s\n", typeid(data.velocityNorth).name());
+        // printf("RAW Velocity North: %d\n", data.velocityNorth);
 
         elapsedTime += updateInterval;
+        // Print Last value in velocityNorth
+        // printf("Last Velocity North: %d\n", velocityNorth.back());
+        // printf("Vector: ");
+        // for (int i = 0; i < velocityNorth.size(); i++) {
+        //     printf("%d , ", velocityNorth[i]);
+        // }
+        // printf("\n");
+
+        plt::figure(1);
+        plt::clf(); // Clear the current figure
+        plt::title("Velocities");
+        plt::named_plot("North Velocity", time, velocityNorth);
+        plt::named_plot("East Velocity", time, velocityEast);
+        plt::legend();
+        plt::pause(0.5);
 
         sleep(1);
     }
 
     return 0;
 }
-
