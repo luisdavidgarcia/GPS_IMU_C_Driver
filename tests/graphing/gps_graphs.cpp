8a56424f9d628c602fabb6f479d80b5d3db84ad7
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-12-01 12:06:18 -0800

    resolved bugs

--- tests/graphing/gps_graphs.cpp
+++ tests/graphing/gps_graphs.cpp
@@ -26,7 +26,7 @@ int main() {
                 velocityEast.erase(velocityEast.begin());
             }
 
-            if ((data.velocityNorth < 500 || data.velocityNorth > -500) & (data.velocityEast < 500 || data.velocityEast > -500)) {
+            if ((data.velocityNorth < 500 || data.velocityNorth > -500) && (data.velocityEast < 500 || data.velocityEast > -500)) {
                 // Update the time and data vectors
                 time.push_back(elapsedTime);
                 velocityNorth.push_back(data.velocityNorth);
@@ -44,7 +44,7 @@ int main() {
                 plt::named_plot("North Velocity", time, velocityNorth);
                 plt::named_plot("East Velocity", time, velocityEast);
                 plt::legend();
-                plt::pause(0.01);
+                plt::pause(0.5);
             }
         }
 
