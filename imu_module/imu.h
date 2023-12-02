c42b05ac955d271648cd7fd70f635e139abd15d1
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-12-02 10:46:21 -0800

    resolved bugs

--- imu_module/imu.h
+++ imu_module/imu.h
@@ -129,7 +129,7 @@ extern "C" {
 #define ACCEL_MG_LSB_8G (0.000976F)
 
 /** Macro for micro tesla (uT) per LSB (1 LSB = 0.1uT) */
-#define MAG_UT_LSB (0.1R)
+#define MAG_UT_LSB (0.1)
 #define MAG_MAX_THRESHOLD 5000 // µT, adjust as needed
 
 class Imu {
