43a5d1f13f0f3714e0853c8cdca76eed6bc72138
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-12-05 13:11:03 -0800

    resolved bugs

--- gps_module/gps.h
+++ gps_module/gps.h
@@ -187,7 +187,7 @@ class Gps {
 		uint8_t navigationRate, uint8_t timeref);
 
 		int16_t i2_to_int(const uint8_t *little_endian_bytes);
-        double bytes_to_double(const uint8_t *little_endian_bytes);
+        // double bytes_to_double(const uint8_t *little_endian_bytes);
 		uint16_t u2_to_int(const uint8_t *little_endian_bytes);
 		int32_t i4_to_int(const uint8_t *little_endian_bytes);
 		uint32_t u4_to_int(const uint8_t *little_endian_bytes);
