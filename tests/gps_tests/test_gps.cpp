178b305f432a966a60c95235b591f094c9b18004
Luis David Garcia <hookeri_bartz@aleeas.com>
2023-12-05 12:39:25 -0800

    resolved bugs

--- tests/gps_tests/test_gps.cpp
+++ tests/gps_tests/test_gps.cpp
@@ -24,6 +24,7 @@ int main(void) {
   while(!exit_flag) {
 
     PVTData data = gps_module.GetPvt(true, 1);
+    printf("Year: %d\n", data.year);
     if (data.year == 2023 & data.numberOfSatellites > 0) {
       /* Use these times to plot on x-axis */
       printf("Year: %d\n", data.year);
