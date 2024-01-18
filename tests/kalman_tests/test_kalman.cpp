21255edd49853e0adf13bf2682b18d6ff98979b3
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-01-18 13:48:44 -0800

    Changes made

--- tests/kalman_tests/test_kalman.cpp
+++ tests/kalman_tests/test_kalman.cpp
@@ -30,9 +30,9 @@ int main(void) {
     float ax, ay, az, gx, gy, gz, hx, hy, hz, pitch, roll, yaw;
 
     while(!exit_flag) {
-        PVTData gps_data = gps_module.GetPvt(true, 1);
+//        PVTData gps_data = gps_module.GetPvt(true, 1);
         imu_module.readSensorData();
-        if (gps_data.year == CURRENT_YEAR && gps_data.numberOfSatellites > 0) {
+//        if (gps_data.year == CURRENT_YEAR && gps_data.numberOfSatellites > 0) {
             const int16_t *accel_data = imu_module.getAccelerometerData();
             if (accel_data[0] == ACCEL_MAX_THRESHOLD && accel_data[1] == ACCEL_MAX_THRESHOLD && accel_data[2] == ACCEL_MAX_THRESHOLD) {
                 printf("Accelerometer data is invalid.\n");
@@ -91,9 +91,9 @@ int main(void) {
             printf("Yaw       : %2.3f %2.3f\n", yaw, ekf.getHeading_rad());
 
             printf("\n---------------------\n");
-        } else {
-            printf("No GPS data\n");
-        }
+//        } else {
+//            printf("No GPS data\n");
+//        }
         sleep(1);
     }
 
