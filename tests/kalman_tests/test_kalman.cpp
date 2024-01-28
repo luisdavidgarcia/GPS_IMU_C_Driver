0044a01720306f356689fe3f5bf97aaac1e0aaf0
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-01-28 11:51:34 -0800

    Apply accelerometer offsets and gyroscope biases, and implement low-pass filters for IMU data

--- tests/kalman_tests/test_kalman.cpp
+++ tests/kalman_tests/test_kalman.cpp
@@ -37,15 +37,23 @@ int main(void) {
     float filteredMx = 0, filteredMy = 0, filteredMz = 0;
     const float alpha = 0.5; // Adjust this parameter to tweak the filter (range: 0-1)
 
-    // Open a file in write mode to log data
-    std::ofstream imuDataFile("tests/kalman_tests/accel_gyro_imu_data.csv");
-    if (imuDataFile.is_open()) {
-        // Write headers to the CSV file
-        imuDataFile << "AccelX,AccelY,AccelZ,GyroX,GyroY,GyroZ,MagX,MagY,MagZ\n";
-    } else {
-        std::cerr << "Unable to open file for writing IMU data." << std::endl;
-        return 1; // Exit if file cannot be opened
-    }
+    // Define the accelerometer offsets and gyroscope biases
+    const float accel_x_offset = 4472.898166403786;
+    const float accel_y_offset = -936.4657925867508;
+    const float accel_z_offset = 996.033714511041;
+    const float gyro_x_bias = -527.8627760252366;
+    const float gyro_y_bias = -286.20908911671927;
+    const float gyro_z_bias = 30.305599369085172;
+
+    // // Open a file in write mode to log data
+    // std::ofstream imuDataFile("tests/kalman_tests/accel_gyro_imu_data.csv");
+    // if (imuDataFile.is_open()) {
+    //     // Write headers to the CSV file
+    //     imuDataFile << "AccelX,AccelY,AccelZ,GyroX,GyroY,GyroZ,MagX,MagY,MagZ\n";
+    // } else {
+    //     std::cerr << "Unable to open file for writing IMU data." << std::endl;
+    //     return 1; // Exit if file cannot be opened
+    // }
 
     while(!exit_flag) {
         // All data for IMU is normalized already for 250dps, 2g, and 4 gauss
@@ -68,56 +76,60 @@ int main(void) {
             continue;
         }
 
-        printf("AccelX: %d, AccelY: %d, AccelZ: %d\n", accel_data[0], accel_data[1], accel_data[2]);
-        printf("GyroX: %d, GyroY: %d, GyroZ: %d\n", gyro_data[0], gyro_data[1], gyro_data[2]);
-        printf("MagX: %d, MagY: %d, MagZ: %d\n", mag_data[0], mag_data[1], mag_data[2]);
+        // printf("AccelX: %d, AccelY: %d, AccelZ: %d\n", accel_data[0], accel_data[1], accel_data[2]);
+        // printf("GyroX: %d, GyroY: %d, GyroZ: %d\n", gyro_data[0], gyro_data[1], gyro_data[2]);
+        // printf("MagX: %d, MagY: %d, MagZ: %d\n", mag_data[0], mag_data[1], mag_data[2]);
 
         // Write the IMU data to the file
-        if (imuDataFile.is_open()) {
-            imuDataFile << accel_data[0] << "," << accel_data[1] << "," << accel_data[2] << ","
-                        << gyro_data[0] << "," << gyro_data[1] << "," << gyro_data[2] << ","
-                        << mag_data[0] << "," << mag_data[1] << "," << mag_data[2] << "\n";
-        }
-
-        // Gxyz[0] = GYRO_SENSITIVITY_250DPS * DEG_TO_RAD * (static_cast<float>(gyro_data[0]));// - G_offset[0]);
-        // Gxyz[1] = GYRO_SENSITIVITY_250DPS * DEG_TO_RAD * (static_cast<float>(gyro_data[1]));// - G_offset[1]);
-        // Gxyz[2] = GYRO_SENSITIVITY_250DPS * DEG_TO_RAD * (static_cast<float>(gyro_data[2]));// - G_offset[2]);
-        // Axyz[0] = static_cast<float>(accel_data[0]) * ACCEL_MG_LSB_2G; //* SENSORS_GRAVITY_STD;
-        // Axyz[1] = -1 * static_cast<float>(accel_data[1]) * ACCEL_MG_LSB_2G; //* SENSORS_GRAVITY_STD;
-        // Axyz[2] = static_cast<float>(accel_data[2]) * ACCEL_MG_LSB_2G; //* SENSORS_GRAVITY_STD;
-        // Mxyz[0] = static_cast<float>(mag_data[0]) * MAG_UT_LSB;;
-        // Mxyz[1] = static_cast<float>(mag_data[1]) * MAG_UT_LSB;;
-        // Mxyz[2] = static_cast<float>(mag_data[2]) * MAG_UT_LSB;;
-
-        // // Low-pass filter for accelerometer data
-        // filteredAx = alpha * filteredAx + (1 - alpha) * Axyz[0];
-        // filteredAy = alpha * filteredAy + (1 - alpha) * Axyz[1];
-        // filteredAz = alpha * filteredAz + (1 - alpha) * Axyz[2];
-
-        // // Low-pass filter for magnetometer data
-        // filteredMx = alpha * filteredMx + (1 - alpha) * Mxyz[0];
-        // filteredMy = alpha * filteredMy + (1 - alpha) * Mxyz[1];
-        // filteredMz = alpha * filteredMz + (1 - alpha) * Mxyz[2];
-
-        // std::tie(pitch,roll,yaw) = ekf.getPitchRollYaw(filteredAx, filteredAy, filteredAz, filteredMx, filteredMy, filteredMz);
-        // printf("Roll 	  : %2.3f\n", ekf.getRoll_rad());
-        // printf("Pitch     : %2.3f\n", ekf.getPitch_rad());
-        // printf("Yaw       : %2.3f\n", ekf.getHeading_rad());
-
-        // // Write only the IMU data to a file
-        // std::ofstream outfile("tests/kalman_tests/rpy_data.txt");
-        // if (outfile.is_open()) {
-        //     outfile << ekf.getRoll_rad() << "," << ekf.getPitch_rad() << "," << ekf.getHeading_rad() << std::endl;
-        //     outfile.flush(); // Flush the stream
-        //     outfile.close(); // Close the file to save the changes
-        // } else {
-        //     std::cerr << "Unable to open file for writing." << std::endl;
+        // if (imuDataFile.is_open()) {
+        //     imuDataFile << accel_data[0] << "," << accel_data[1] << "," << accel_data[2] << ","
+        //                 << gyro_data[0] << "," << gyro_data[1] << "," << gyro_data[2] << ","
+        //                 << mag_data[0] << "," << mag_data[1] << "," << mag_data[2] << "\n";
         // }
+
+        // Apply accelerometer offsets
+        Axyz[0] = (static_cast<float>(accel_data[0]) - accel_x_offset) * ACCEL_MG_LSB_2G;
+        Axyz[1] = (-1 * static_cast<float>(accel_data[1]) - accel_y_offset) * ACCEL_MG_LSB_2G;
+        Axyz[2] = (static_cast<float>(accel_data[2]) - accel_z_offset) * ACCEL_MG_LSB_2G;
+
+        // Apply gyroscope biases
+        Gxyz[0] = (GYRO_SENSITIVITY_250DPS * DEG_TO_RAD * (static_cast<float>(gyro_data[0])) - gyro_x_bias);
+        Gxyz[1] = (GYRO_SENSITIVITY_250DPS * DEG_TO_RAD * (static_cast<float>(gyro_data[1])) - gyro_y_bias);
+        Gxyz[2] = (GYRO_SENSITIVITY_250DPS * DEG_TO_RAD * (static_cast<float>(gyro_data[2])) - gyro_z_bias);
+
+        Mxyz[0] = static_cast<float>(mag_data[0]) * MAG_UT_LSB;;
+        Mxyz[1] = static_cast<float>(mag_data[1]) * MAG_UT_LSB;;
+        Mxyz[2] = static_cast<float>(mag_data[2]) * MAG_UT_LSB;;
+
+        // Low-pass filter for accelerometer data
+        filteredAx = alpha * filteredAx + (1 - alpha) * Axyz[0];
+        filteredAy = alpha * filteredAy + (1 - alpha) * Axyz[1];
+        filteredAz = alpha * filteredAz + (1 - alpha) * Axyz[2];
+
+        // Low-pass filter for magnetometer data
+        filteredMx = alpha * filteredMx + (1 - alpha) * Mxyz[0];
+        filteredMy = alpha * filteredMy + (1 - alpha) * Mxyz[1];
+        filteredMz = alpha * filteredMz + (1 - alpha) * Mxyz[2];
+
+        std::tie(pitch,roll,yaw) = ekf.getPitchRollYaw(filteredAx, filteredAy, filteredAz, filteredMx, filteredMy, filteredMz);
+        printf("Roll 	  : %2.3f\n", ekf.getRoll_rad());
+        printf("Pitch     : %2.3f\n", ekf.getPitch_rad());
+        printf("Yaw       : %2.3f\n", ekf.getHeading_rad());
+
+        // Write only the IMU data to a file
+        std::ofstream outfile("tests/kalman_tests/rpy_data.txt");
+        if (outfile.is_open()) {
+            outfile << ekf.getRoll_rad() << "," << ekf.getPitch_rad() << "," << ekf.getHeading_rad() << std::endl;
+            outfile.flush(); // Flush the stream
+            outfile.close(); // Close the file to save the changes
+        } else {
+            std::cerr << "Unable to open file for writing." << std::endl;
+        }
 
         // printf("\n---------------------\n");
         sleep(0.5);
     }
 
-    imuDataFile.close(); // Close the file when done
+    // imuDataFile.close(); // Close the file when done
     return 0;
 }
