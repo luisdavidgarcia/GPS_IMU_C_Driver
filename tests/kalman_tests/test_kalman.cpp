58ed6d79906096a96d9ae5245c00e8aa81b9e248
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-01-30 13:57:46 -0800

    updated test

--- tests/kalman_tests/test_kalman.cpp
+++ tests/kalman_tests/test_kalman.cpp
@@ -124,8 +124,7 @@ int main(void) {
             // Write only the IMU data to a file
             std::ofstream outfile("tests/kalman_tests/rpy_data.txt");
             if (outfile.is_open()) {
-                outfile << ekf.getPitch_rad() << "," << ekf.getRoll_rad() << "," << ekf.getHeading_rad() << data.latitude << "," 
-                << data.longitude << "," << data.height << std::endl;
+                outfile << ekf.getPitch_rad() << "," << ekf.getRoll_rad() << "," << ekf.getHeading_rad() << "," << data.latitude << "," << data.longitude << "," << data.height << std::endl;
                 outfile.flush(); // Flush the stream
                 outfile.close(); // Close the file to save the changes
             } else {
