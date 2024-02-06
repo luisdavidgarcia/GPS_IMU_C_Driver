c768604b53c73c36dd734da4e9b399dc7a822830
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-02-06 12:41:43 -0800

    Update setup.sh: Comment out unnecessary library installations and remove unnecessary commands

--- setup.sh
+++ setup.sh
@@ -24,37 +24,40 @@ sudo apt install libserialport-dev
 # Install Eigen Library
 sudo apt install libeigen3-dev
 
-# Install JPEG and TIFF libraries
-sudo apt install libjpeg-dev libtiff5-dev
+###############################################
+# ONLY NECESSARY FOR PLOTTING
+###############################################
+# # Install JPEG and TIFF libraries
+# sudo apt install libjpeg-dev libtiff5-dev
 
-# Install BLAS and LAPACK libraries
-sudo apt install libblas-dev liblapack-dev
+# # Install BLAS and LAPACK libraries
+# sudo apt install libblas-dev liblapack-dev
 
-# Install a 64-bit ARM Cross-Compiler:
-sudo apt-get install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+# # Install a 64-bit ARM Cross-Compiler:
+# sudo apt-get install gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
 
-# Install FFTW library
-sudo apt install libfftw3-dev
+# # Install FFTW library
+# sudo apt install libfftw3-dev
 
-# Clone the Matplot++ repository
-git clone https://github.com/alandefreitas/matplotplusplus.git
+# # Clone the Matplot++ repository
+# git clone https://github.com/alandefreitas/matplotplusplus.git
 
-# Navigate to the Matplot++ directory
-cd matplotplusplus
+# # Navigate to the Matplot++ directory
+# cd matplotplusplus
 
-# Create a build directory
-mkdir build
+# # Create a build directory
+# mkdir build
 
-# Enter the build directory
-cd build
+# # Enter the build directory
+# cd build
 
-# Configure and build Matplot++
-cmake .. -DCMAKE_CXX_FLAGS="-O2" -DMATPLOTPP_BUILD_EXAMPLES=OFF -DMATPLOTPP_BUILD_TESTS=OFF
-sudo cmake --build .
-sudo cmake --install .
+# # Configure and build Matplot++
+# cmake .. -DCMAKE_CXX_FLAGS="-O2" -DMATPLOTPP_BUILD_EXAMPLES=OFF -DMATPLOTPP_BUILD_TESTS=OFF
+# sudo cmake --build .
+# sudo cmake --install .
 
-# Install Matplot++ system-wide
-sudo make install
+# # Install Matplot++ system-wide
+# sudo make install
 
-# Install the MatplotLib C++ header
-wget https://raw.githubusercontent.com/lava/matplotlib-cpp/master/matplotlibcpp.h
+# # Install the MatplotLib C++ header
+# wget https://raw.githubusercontent.com/lava/matplotlib-cpp/master/matplotlibcpp.h
