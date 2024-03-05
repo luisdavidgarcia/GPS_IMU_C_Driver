5e7aa0de9f8a5ea9bf3df9501189ce3ac279c4d4
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-03-05 14:14:04 -0800

    Fix printf statement in test_kalman.cpp

--- tests/kalman_tests/test_kalman.cpp
+++ tests/kalman_tests/test_kalman.cpp
@@ -120,7 +120,7 @@ int main(void) {
             std::tie(pitch, roll, yaw) = ekf.getPitchRollYaw(filteredAx, filteredAy, filteredAz, Gxyz[0], Gxyz[1], Gxyz[2], filteredMx, filteredMy, filteredMz, dt);
             printf("Pitch: %2.3f, Roll: %2.3f, Yaw: %2.3f\n", pitch, roll, yaw);
             // Print Lat, Long, and Height
-            printf("Latitude: %f, Longitude: %f\n,", data.latitude, data.longitude);
+            printf("Latitude: %f, Longitude: %f\n", data.latitude, data.longitude);
             //, Height: %d\n", data.latitude, data.longitude, data.height);
 
             // Write only the IMU data to a file
