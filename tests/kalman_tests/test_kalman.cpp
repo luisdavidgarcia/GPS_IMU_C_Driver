3d6e9a3382b5f2677a5179ee3d62e756118064ab
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-03-07 13:09:23 -0800

    Remove unused files and update sleep time in test_kalman.cpp

--- tests/kalman_tests/test_kalman.cpp
+++ tests/kalman_tests/test_kalman.cpp
@@ -137,8 +137,7 @@ int main(void) {
             }
 
             printf("\n---------------------\n");
-            // sleep(1);
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(110));
         }
     }
 
