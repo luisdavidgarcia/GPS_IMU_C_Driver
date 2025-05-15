edfbb35d2152a365d88e811dc62c341159fc624c
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-05-15 09:12:36 -0700

    Reduced to run soley on a dockerfile for simplicity

--- setup.sh
+++ setup.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-# setup virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-pip3 install matplotlib
-pip3 install numpy
-
-# Update package lists
-apt update
-
-# Install C++ development tools
-apt install -y build-essential cmake
-
-# Install I2C libraries
-apt install -y libi2c-dev
-
-# Install Eigen Library
-apt install -y libeigen3-dev
+docker build -t gps_imu_dev_env .
