228e28543f0a9271648bcc85b6f9a73c4c39644b
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-05-30 00:42:27 -0700

    Refactor UBX message composition: improve parameter naming, update member variable access, and enhance string return values

--- src/ubx/ubx_msg.cpp
+++ src/ubx/ubx_msg.cpp
@@ -1,11 +1,9 @@
 #include "UBX/UBX_MSG.hpp"
 
 /**
- * @brief   Compose a UBX message with the given message class, message ID, optional payloadLength, and payload.
- * @param   msg_class   The message class of the UBX message.
- * @param   msg_id      The message ID of the UBX message.
- * @param   payloadLength      The payloadLength of the payload (default is 0).
- * @param   payload     The payload data (default is nullptr).
+ * @brief   Compose a UBX message.
+ * @param   messageInfo   The message class and ID of the UBX message.
+ * @param   payload       The payload data (default is nullptr).
  * @return  The composed UBX message.
  */
 void UBX::ComposeMessage(
@@ -14,19 +12,19 @@ void UBX::ComposeMessage(
 {
   ResetPayload();
 
-  message.sync1 = SYNC_CHAR_1;
-  message.sync2 = SYNC_CHAR_2;
-  message.msgClass = messageInfo.msgClass;
-  message.msgId = messageInfo.msgId;
-  message.payloadLength = std::min(
+  message_.sync1 = SYNC_CHAR_1;
+  message_.sync2 = SYNC_CHAR_2;
+  message_.msgClass = messageInfo.msgClass;
+  message_.msgId = messageInfo.msgId;
+  message_.payloadLength = std::min(
     static_cast<uint16_t>(payload.size()), 
     static_cast<uint16_t>(MAX_PAYLOAD_LENGTH)
   );
 
   std::copy(
     payload.begin(), 
-    payload.begin() + message.payloadLength, 
-    std::begin(message.payload));
+    payload.begin() + message_.payloadLength, 
+    std::begin(message_.payload));
 
   ComputeChecksum();
 }
@@ -39,49 +37,49 @@ void UBX::ComposeMessage(
 std::string MsgClassToString(const uint8_t& msgClass) 
 {
   if (msgClass == NAV_CLASS) {
-    return "Navigation";
+    return "Navigation"s;
   }
   if (msgClass == RXM_CLASS) {
-    return "Receiver Manager";
+    return "Receiver Manager"s;
   }
   if (msgClass == INF_CLASS) {
-    return "Information";
+    return "Information"s;
   }
   if (msgClass == ACK_CLASS) {
-    return "ACK/NAK";
+    return "ACK/NAK"s;
   }
   if (msgClass == CFG_CLASS) {
-    return "Configuration";
+    return "Configuration"s;
   }
   if (msgClass == UPD_CLASS) {
-    return "Firmware update";
+    return "Firmware update"s;
   }
   if (msgClass == MON_CLASS) {
-    return "Monitoring";
+    return "Monitoring"s;
   }
   if (msgClass == AID_CLASS) {
-    return "AssistNow messages";
+    return "AssistNow messages"s;
   } 
   if (msgClass == TIM_CLASS) { 
-    return "Timing";
+    return "Timing"s;
   }
   if (msgClass == ESF_CLASS) {
-    return "External Sensor Fusion Messages";
+    return "External Sensor Fusion Messages"s;
   }
   if (msgClass == MGA_CLASS) {
-    return "Multiple GNSS Assistance Messages";
+    return "Multiple GNSS Assistance Messages"s;
   }
   if (msgClass == LOG_CLASS) {
-    return "Logging";
+    return "Logging"s;
   }
   if (msgClass == SEC_CLASS) {
-    return "Security";
+    return "Security"s;
   }
   if (msgClass == HNR_CLASS) {
-    return "High rate navigation results";
+    return "High rate navigation results"s;
   }
 
-  return "Couldn't find class";
+  return "Couldn't find class"s;
 }
 
 /**
@@ -115,52 +113,49 @@ std::string GetGNSSFixType(const uint8_t& fixFlag)
 
 /**
  * @brief   Calculate the checksum values (checksumA and checksumB) for a UBX message.
- * @param   msg The UbxMessage struct for which to calculate the checksum.
  */
 void UBX::ComputeChecksum() 
 {
-  message.checksumA = message.msgClass;
-  message.checksumB = message.checksumA;
+  message_.checksumA = message_.msgClass;
+  message_.checksumB = message_.checksumA;
 
-  message.checksumA += message.msgId;
-  message.checksumB += message.checksumA;
+  message_.checksumA += message_.msgId;
+  message_.checksumB += message_.checksumA;
 
-  message.checksumA += message.payloadLength % (1 << 8);
-  message.checksumB += message.checksumA;
+  message_.checksumA += message_.payloadLength % (1 << 8);
+  message_.checksumB += message_.checksumA;
 
-  message.checksumA += message.payloadLength >> 8;
-  message.checksumB += message.checksumA;
+  message_.checksumA += message_.payloadLength >> 8;
+  message_.checksumB += message_.checksumA;
 
-  for (int i = 0; i < message.payloadLength; i++) {
-    message.checksumA += message.payload.at(i);
-    message.checksumB += message.checksumA;
+  for (int i = 0; i < message_.payloadLength; i++) {
+    message_.checksumA += message_.payload.at(i);
+    message_.checksumB += message_.checksumA;
   }
 }
 
 /**
  * @brief   Reset the payload of a UBX message to all zeros.
- * @param   msg The UbxMessage struct for which to reset the payload.
  */
 void UBX::ResetPayload() 
 {
-  std::fill(message.payload.begin(), message.payload.end(), 0);
+  std::fill(message_.payload.begin(), message_.payload.end(), 0);
 }
 
 /**
  * @brief   Convert a UBX message to its string representation.
- * @param   msg The UbxMessage struct to convert.
  * @return  The string representation of the UBX message.
  */
 std::string UBX::UbxMessageToString() const {
-  std::string result = "Class : " + std::to_string(message.msgClass) + "\n";
-  result += "ID : " + std::to_string(message.msgId) + "\n";
-  result += "Length : " + std::to_string(message.payloadLength) + "\n";
+  std::string result = "Class : " + std::to_string(message_.msgClass) + "\n";
+  result += "ID : " + std::to_string(message_.msgId) + "\n";
+  result += "Length : " + std::to_string(message_.payloadLength) + "\n";
   result += "Payload : ";
-  for (int i = 0; i < message.payloadLength; i++) {
-    result += std::to_string(message.payload.at(i)) + " ";
+  for (int i = 0; i < message_.payloadLength; i++) {
+    result += std::to_string(message_.payload.at(i)) + " ";
   }
   result += "\n";
-  result += "checksum : " + std::to_string(message.checksumA) + " " + std::to_string(message.checksumB) + "\n";
+  result += "checksum : " + std::to_string(message_.checksumA) + " " + std::to_string(message_.checksumB) + "\n";
   return result;
 }
 
